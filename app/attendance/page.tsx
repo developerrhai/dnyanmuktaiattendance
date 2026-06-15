faa9f9ecfd9f628972681de8c33f8bd80ba6eba8
Vaibhav Kandharkar <vaibhavk0099#gmail.com>
2026-06-15 15:48:52 +0530

    first commit

--- app/attendance/page.tsx
+++ app/attendance/page.tsx
@@ -230,4 +230,4 @@ export default function AttendancePage() {
       />
     </div>
   );
-}
+}                                                                                                                                     
